2e3c58eff5061c313c2a09fc5d7e918f65269810
codefuncode <codefuncode@protonmail.com>
2020-05-13 21:03:15 -0400

    MOdulo 10 completado  y video publicado

---
+++ 01-Tutorial-HTML/010-Elementos-de-citas- HTML/Ejemplos/index2.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8"/>
+    <title>
+      Document
+    </title>
+  </head>
+  <body>
+    <p>
+      hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+      <q>
+        consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      </q>
+    </p>
+    <hr/>
+    <p>
+      222222222222222222222222
+      <cite>
+        (Sosa, Giles y Carrilla, 1990).
+      </cite>
+    </p>
+    <hr/>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    </p>
+    <blockquote>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </blockquote>
+    <abbr title="World Health Organization">
+      WHO
+    </abbr>
+    <hr/>
+    <bdo dir="rtl">
+      This text will be written from right to left
+    </bdo>
+    <p>
+      This text will be written from right to left
+    </p>
+    <hr/>
+    <address>
+      Tutor del código divertido
+      <br/>
+      # teléfono
+      <br/>
+      123-123-1234
+      <br/>
+      Box 564, Disneyland
+      <br/>
+      USA
+      <br/>
+    </address>
+  </body>
+</html>
