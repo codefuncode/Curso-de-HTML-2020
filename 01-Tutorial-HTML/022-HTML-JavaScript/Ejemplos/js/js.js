c051b9d761b99eba9326986be25868ef550958ec
codefuncode <codefuncode@protonmail.com>
2020-06-05 10:36:29 -0400

    MOdulo 22 completado y video publicado

--- 01-Tutorial-HTML/022-HTML-JavaScript/Ejemplos/js/js.js
+++ 01-Tutorial-HTML/022-HTML-JavaScript/Ejemplos/js/js.js
@@ -1,1 +1,12 @@
-document.getElementById("demo").innerHTML = "JavaScript!";
+var parr = document.getElementById('parr');
+
+parr.setAttribute(
+    "style",
+    "color: green;" +
+    "font-weight: bold;" +
+    "font-size: 1.5em;" +
+    "border: 1px solid green;" +
+    "padding: 25px;"
+);
+
+parr.innerHTML = "Felicidades JavaScript esta funcionando!";
